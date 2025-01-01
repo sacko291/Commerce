a0d817f354aae6e0b82a44b7a1b6a6fa7c047568
sacko291 <moussasacko418@outlook.fr>
2025-01-01 16:32:47 +0100

    Update command.tsx

--- components/sidebar/command.tsx
+++ components/sidebar/command.tsx
@@ -5,6 +5,7 @@ import {
     CommandList,
     CommandSeparator,
   } from "@/components/ui/command"
+  import  Link  from "next/link"
 
 export const CommandLateral = ({siteBarMenu}: {siteBarMenu: any}) => {
   return (
@@ -12,9 +13,9 @@ export const CommandLateral = ({siteBarMenu}: {siteBarMenu: any}) => {
             <CommandList className="max-h-screen h-full mb-5 no-scrollbar">
                 {siteBarMenu.map((option: any, key: number) => (
                     <CommandGroup key={key} className="m-2">
-                                <a className="flex hover:bg-cyan-500 hover:text-white rounded-md" key={key} href={option.url}>
+                                <Link className="flex hover:bg-cyan-500 hover:text-white rounded-md" key={key} href={option.url}>
                                     <CommandItem>{option.text}</CommandItem>
-                                </a>                  
+                                </Link>                  
                         <CommandSeparator />
                     </CommandGroup>
 
